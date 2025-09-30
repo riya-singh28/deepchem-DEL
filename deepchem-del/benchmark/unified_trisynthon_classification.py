62455ee8f3e763c3ca45063d27d844e9a081da03
Riya Singh <riyasingh3909@gmail.com>
2025-09-30 23:01:16 +0530

    update uni_tri_class_pipeline

--- deepchem-del/benchmark/unified_trisynthon_classification.py
+++ deepchem-del/benchmark/unified_trisynthon_classification.py
@@ -1,21 +1,27 @@
 """
 Unified Trisynthon Classification Pipeline.
 """
-
 import json
+import yaml
 import argparse
 import pandas as pd
-from utils.pipeline import Pipeline
+from datetime import datetime
 from typing import List, Dict, Any, Optional
+from utils.pipeline import Pipeline
 from utils.logging_utils import setup_logging
 from utils.denoise_utils import calculate_poisson_enrichment, calculate_hit_threshold
 
 logger = setup_logging(__name__)
 
+
 def denoise_data(file_path: str,
                  output_file_path: str,
-                 control_cols: List[str] = ['seq_matrix_1', 'seq_matrix_2', 'seq_matrix_3'],
-                 target_cols: List[str] = ['seq_target_1', 'seq_target_2', 'seq_target_3'],
+                 control_cols: List[str] = [
+                     'seq_matrix_1', 'seq_matrix_2', 'seq_matrix_3'
+                 ],
+                 target_cols: List[str] = [
+                     'seq_target_1', 'seq_target_2', 'seq_target_3'
+                 ],
                  percentile: float = 90) -> str:
     """
     Denoise and preprocess trisynthon data for classification analysis.
@@ -66,17 +72,28 @@ def denoise_data(file_path: str,
     >>> denoised_file_path = denoise_data("example_input.csv", "example_output.csv", control_cols, target_cols, percentile=90)
     """
     data = pd.read_csv(file_path)
-    data = data.dropna(subset=['smiles'])
-    data = data.drop_duplicates(subset=['smiles'])
+    data_clean = data.dropna(subset=['smiles'])
+    dropped_na = data[~data.index.isin(data_clean.index)]
+    logger.info("Dropped rows (dropna)", extra={"rows": len(dropped_na)})
+
+    data_unique = data_clean.drop_duplicates(subset=['smiles'])
+    dropped_duplicates = data_clean[~data_clean.index.isin(data_unique.index)]
+    logger.info("Dropped rows (drop duplicates)",
+                extra={"rows": len(dropped_duplicates)})
 
     # calculate poisson enrichment
-    data = calculate_poisson_enrichment(df=data, control_cols=control_cols, target_cols=target_cols)
-    target_threshold = calculate_hit_threshold(df=data, column_name='Poisson_Enrichment', percentile=percentile)
+    data = calculate_poisson_enrichment(df=data_unique,
+                                        control_cols=control_cols,
+                                        target_cols=target_cols)
+    target_threshold = calculate_hit_threshold(
+        df=data, column_name='Poisson_Enrichment', percentile=percentile)
     data['hits'] = (data['Poisson_Enrichment'] > target_threshold).astype(int)
     data.to_csv(output_file_path, index=False)
     return output_file_path
 
-def unified_trisynthon_classification_pipeline(config: Dict[str, Any]) -> Dict[str, Any]:
+
+def unified_trisynthon_classification_pipeline(
+        config: Dict[str, Any]) -> Dict[str, Any]:
     """
     Execute the unified trisynthon classification pipeline.
 
@@ -103,34 +120,41 @@ def unified_trisynthon_classification_pipeline(config: Dict[str, Any]) -> Dict[s
     ...     config = json.load(f)
     >>> unified_trisynthon_classification_pipeline(config)
     """
-    denoised_file_path = denoise_data(file_path=config['denoise_config']['file_path'],
-                                      output_file_path=config['denoise_config']['denoised_file_path'],
-                                      control_cols=config['denoise_config']['control_cols'],
-                                      target_cols=config['denoise_config']['target_cols'],
-                                      percentile=config['denoise_config']['hit_percentile'])
-    config['files_to_upload']['file_path'] = denoised_file_path
-    logger.info("Denoised file path set for upload", extra={"file_path": config['files_to_upload']['file_path']})
+    denoised_file_path = denoise_data(
+        file_path=config['denoise_config']['file_path'],
+        output_file_path=config['denoise_config']['denoised_file_path'],
+        control_cols=config['denoise_config']['control_cols'],
+        target_cols=config['denoise_config']['target_cols'],
+        percentile=config['denoise_config']['hit_percentile'])
+    config['files_to_upload'].append(denoised_file_path)
+    logger.info("Denoised file path set for upload",
+                extra={"file_path": config['files_to_upload'][-1]})
     pipe = Pipeline(config)
     result = pipe.run()
     return result
+
 
 def main(args: argparse.Namespace) -> Optional[Dict[str, Any]]:
     """
     Main entry point for the unified trisynthon classification pipeline.
     """
     with open(args.config, 'r') as f:
-        config = json.load(f)
-
+        config = yaml.safe_load(f)
     result = unified_trisynthon_classification_pipeline(config)
-    logger.info("Pipeline result ready", extra={"result": result})
-    # save result to json
-    with open('result.json', 'w') as f:
+
+    result_file_name = f"{result['run_id']}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+    with open(result_file_name, 'w') as f:
         json.dump(result, f)
-    logger.info("Pipeline result ready", extra={"result_file": 'result.json'})
-    return result
+    logger.info("Pipeline result ready",
+                extra={"result_file": result_file_name})
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, required=True)
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to YAML configuration file containing pipeline parameters")
     args = parser.parse_args()
     main(args)
