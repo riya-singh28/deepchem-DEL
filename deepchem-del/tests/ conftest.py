b22c8482fab670cb447487914edd496bfd4ca195
Riya Singh <riyasingh3909@gmail.com>
2025-09-30 20:32:19 +0530

    add conftest and fix pipeline test

---
+++ deepchem-del/tests/ conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+
+@pytest.fixture
+def pipeline_config() -> dict:
+    """Provide a reusable pipeline configuration for tests."""
+    return {
+        "settings_file": "tests/assets/test_settings.json",
+        "files_to_upload": ["tests/assets/test_dataset.csv"],
+        "featurizer_config": {
+            "featurizer": "ecfp",
+            "output": "feat_out_4",
+            "dataset_column": "smiles",
+            "label_column": "Poisson_Enrichment",
+        },
+        "split_config": {
+            "splitter_type": "random",
+            "frac_train": 0.8,
+            "frac_valid": 0.1,
+            "frac_test": 0.1
+        },
+        "train_config": {
+            "model_type": "random_forest_regressor",
+            "model_name": "test_rf_reg_try4",
+            "init_kwargs": {"n_jobs": -1},
+            "train_kwargs": {}
+        },
+        "evaluate_config": {
+            "metrics": ["rms_score"],
+            "output_key": "test_evaluation_result_4",
+            "is_metric_plots": False
+        },
+        "infer_config": {
+            "output": "test_inference_result_4",
+            "dataset_column": "smiles",
+            "label_column": "Poisson_Enrichment"
+        }
+    }
--- deepchem-del/tests/test_pipeline.py
+++ deepchem-del/tests/test_pipeline.py
@@ -1,55 +1,20 @@
 from utils.pipeline import Pipeline
 
 
-def test_pipeline_end_to_end() -> None:
+def test_pipeline_end_to_end(pipeline_config):
     """End-to-end pipeline test using a temporary JSON config.
     """
-    config_file = {
-        "files_to_upload": {
-            "file_path": "tests/assets/test_dataset.csv",
-            "filename": "name",
-            "description": "desc",
-        },
-        "featurizer_config": {
-            "featurizer": "circular_fps",
-            "output": "feat_out",
-            "dataset_column": "smiles",
-            "label_column": "y",
-        },
-        "split_config": {
-            "splitter_type": "random",
-            "frac_train": 0.8,
-            "frac_valid": 0.1,
-            "frac_test": 0.1
-        },
-        "train_config": {
-            "model_type": "random_forest_regressor",
-            "model_name": "mapk14_1M_rf_reg_try1",
-            "init_kwargs": {"n_jobs": -1},
-            "train_kwargs": {}
-        },
-        "evaluate_config": {
-            "metrics": ["rms_score"],
-            "output_key": "mapk14_1M_evaluation_result",
-            "is_metric_plots": False
-        },
-        "infer_config": {
-            "output": "mapk14_1M_inference_result",
-            "dataset_column": "smiles",
-            "shard_size": 8192,
-            "threshold": 0.5
-        }
-    }
-    p = Pipeline(config_file)
+    p = Pipeline(pipeline_config)
 
     result = p.run()
+
+    tvt_split_address = result["train_valid_test_split_address"]
+    assert len(tvt_split_address['train_valid_test_split_results_address']) == 3
     
     assert set(result.keys()) == {
         "uploaded_files",
         "featurized_dataset_address",
-        "train_split_address",
-        "val_split_address",
-        "test_split_address",
+        "train_valid_test_split_address",
         "trained_model_address",
         "evaluation_result_address",
         "inference_result_address",
